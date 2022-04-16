8e3d4aa2040f7c1e18581fdc7cb6f3e19d5b9d3a
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-16 19:10:35 +0530

    display computerChoice

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const resultDisplay = document.querySelector("#result");
 
 const possibleChoices = document.querySelectorAll("button");
 
-let userChoice
+let userChoice;
+let computerChoice;
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => {
     userChoice = event.target.id;
@@ -14,5 +15,16 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
 
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNumber)
+    // console.log(randomNumber)
+
+    if (randomNumber === 1) {
+        computerChoice = "Rock";
+    }
+    if (randomNumber === 2) {
+        computerChoice = "Paper";
+    }
+    if (randomNumber === 3) {
+        computerChoice = "Scissors";
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
 }
